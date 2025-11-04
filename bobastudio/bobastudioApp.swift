215f96c7d74baf14f4f06f7568098ded303503e2
thxlion <zvkarry@gmail.com>
2025-11-04 00:44:05 +0000

    Initial Commit

---
+++ bobastudio/bobastudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  bobastudioApp.swift
+//  bobastudio
+//
+//  Created by sulley on 04/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct bobastudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bobastudioTests/bobastudioTests.swift
@@ -0,0 +1,17 @@
+//
+//  bobastudioTests.swift
+//  bobastudioTests
+//
+//  Created by sulley on 04/11/2025.
+//
+
+import Testing
+@testable import bobastudio
+
+struct bobastudioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
